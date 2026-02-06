fbdbf8233cc90c5917396240605ced5269593f7a
eduardo <yuunoev@gmail.com>
2026-02-06 17:04:32 -0300

    2

--- src/components/CalcPercent.tsx
+++ src/components/CalcPercent.tsx
@@ -1,9 +1,8 @@
 import {ChevronRight, TrendingUp, TrendingDown} from "lucide-react";
 
-export function CalcPercent({lastMonthValue, currentValue}: {lastMonthValue: number, currentValue: number}) {
+export function CalcPercent({ expense, lastMonthValue, currentValue}: {expense: boolean, lastMonthValue: number, currentValue: number}) {
     function calcDiff() {
-        if (lastMonthValue === 0) return
-2000.00%) return currentValue === 0 ? 0 : 100
+        if (lastMonthValue === 0) return currentValue === 0 ? 0 : 100
         return ((currentValue - lastMonthValue) / lastMonthValue) * 100
     }
     const difference = calcDiff()
@@ -17,19 +16,24 @@ export function CalcPercent({lastMonthValue, currentValue}: {lastMonthValue: num
         return <ChevronRight />
     }
     function setStyle(){
+        if (expense){
+            if(difference === 0){
+                return "text-yellow-100";
+            }
+            if(difference > 0){
+                return "text-[#FF2C2C]"
+            }
+            return "text-[#5CE65C]";
+        }
         if(difference === 0){
-            return "bg-blue-100";
+            return "text-yellow-100";
         }
         if(difference > 0){
-            return "bg-blue-300"
+            return "text-[#5CE65C]"
         }
-        return "bg-blue-400";
+        return "text-[#FF2C2C]";
     }
     return(
-        <div>
-            <span className={`${setStyle()}`}>
-                <span>{difference.toFixed(2)}%{setIcon()}</span>
-            </span>
-        </div>
+        <span className={`bg-[#27272a] inline-flex rounded-md p-0.5`}>{difference.toFixed(2)}%<span className={`${setStyle()}`}>{setIcon()}</span></span>
     )
 }
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -22,26 +22,29 @@ export function Dashboard(){
     const outline = "outline outline-[#3c4147]"
     const cardStyle = "bg-[#393938] rounded-md p-4"
     return(
-        <div>
-            <button className={`${outline} p-3 text-white bg-[#202020] rounded-md `} onClick={()=> setIsOpen(true)}>Adicionar lançamento</button>
+        <div className={"h-dvh bg-[#202020]"}>
             <div className={"p-2 text-white bg-[#202020] flex outline outline-[#3c4147] rounded-md gap-5 justify-around w-full"}>
                 <div className={`${cardStyle}`}>
                     <h2>Patrimônio total: <span>R${totalMoney(jsonStoredTransactions)}</span></h2>
                 </div>
                 <div className={`${cardStyle}`}>
-                    <h2>Receita mensal: <span>R${monthIncome(jsonStoredTransactions)}<CalcPercent currentValue={monthIncome(jsonStoredTransactions)} lastMonthValue={lastMonthIncome(jsonStoredTransactions)} />{}</span></h2>
+                    <h2>Receita mensal: <span>R${monthIncome(jsonStoredTransactions)} <CalcPercent expense={false} currentValue={monthIncome(jsonStoredTransactions)} lastMonthValue={lastMonthIncome(jsonStoredTransactions)} />{}</span></h2>
                     <h3>Receita do mês anterior: <span>R${lastMonthIncome(jsonStoredTransactions)}</span></h3>
                 </div>
                 <div className={`${cardStyle}`}>
-                    <h2>Despesa mensal: <span>R${monthExpense(jsonStoredTransactions)}</span></h2>
+                    <h2>Despesa mensal: <span>R${monthExpense(jsonStoredTransactions)} <CalcPercent expense={true} currentValue={monthExpense(jsonStoredTransactions)} lastMonthValue={lastMonthExpense(jsonStoredTransactions)}/></span></h2>
                     <h3>Despesa do mês anterior: <span>R${lastMonthExpense(jsonStoredTransactions)}</span></h3>
                 </div>
                 <div className={`${cardStyle}`}>
-                    <h2>Balanço mensal: <span>R${monthBal(jsonStoredTransactions)}</span></h2>
+                    <h2>Balanço mensal: <span>R${monthBal(jsonStoredTransactions)} <CalcPercent expense={false} currentValue={monthBal(jsonStoredTransactions)} lastMonthValue={lastMonthBal(jsonStoredTransactions)} /></span></h2>
                     <h3>Balanço do mês anterior: <span>R${lastMonthBal(jsonStoredTransactions)}</span></h3>
                 </div>
             </div>
+            <button className={`${outline} p-3 text-white bg-[#202020] rounded-md cursor-pointer`} onClick={()=> setIsOpen(true)}>Adicionar lançamento</button>
+            <div>
+                <h2>Histórico de lançamentos</h2>
 
+            </div>
             {isOpen && <AddTransaction setIsOpen={setIsOpen}/>}
         </div>
     )
